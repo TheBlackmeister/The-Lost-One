45a96d684fe6cac06a74e8580840c4629fb712a1
cernym68 <cernym68@fel.cvut.cz>
2023-03-29 18:09:08 +0200

    vytvoreni okna

--- src/main/java/cz/cvut/fel/pjv/Main.java
+++ src/main/java/cz/cvut/fel/pjv/Main.java
@@ -1,10 +1,14 @@
 package cz.cvut.fel.pjv;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("BRUH!");
-        System.out.println("BRUH!");
-        System.out.println("BRUH!");
+        JFrame window = new JFrame(); // nove okno - hlavni okno
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // aby slo zavrit
+        window.setResizable(false); // nemenna velikost
+        window.setTitle("Tank Adventure"); // nazev okna
+        window.setLocationRelativeTo(null); // bude otevreno uprostred obrazovky
+        window.setVisible(true);
     }
 }
