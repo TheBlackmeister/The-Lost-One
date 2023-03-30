66edc9fdd81ffc230b47a78079e5cc1f3f26075c
vitcech <cechvit1@fel.cvut.cz>
2023-03-30 19:44:46 +0200

    test - změna barvy

--- src/main/java/cz/cvut/fel/pjv/Controller/GamePanel.java
+++ src/main/java/cz/cvut/fel/pjv/Controller/GamePanel.java
@@ -31,7 +31,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
-        this.setBackground(Color.black);
+        this.setBackground(Color.gray);
         this.setDoubleBuffered(true);
         this.addKeyListener(keyHand);
         this.setFocusable(true);
