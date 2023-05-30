b674d452acb7abdefc903b3ba8daed88828cfc55
cernym68 <cernym68@fel.cvut.cz>
2023-05-30 16:09:34 +0200

    archived JavaDoc, minor changes to the code
    Took 27 minutes

--- src/main/java/cz/cvut/fel/pjv/Controller/Main.java
+++ src/main/java/cz/cvut/fel/pjv/Controller/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
         if(args.length != 0 && args[0].equals("OFF")){
             LogManager.getLogManager().reset();
+
         }
         logger.info("Starting launcher");
         new Launcher();
